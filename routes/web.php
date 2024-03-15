6239cd5d693e2a2522ebed6360b55dfe91a237a8
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-03-15 17:41:35 +0300

    Add the routes to use with the application

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InvoiceController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::group(['prefix' => 'api/v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function(){
+    Route::apiResource('customer', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
 });
